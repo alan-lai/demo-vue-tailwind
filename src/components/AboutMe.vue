20ed5e0c2417752baec191cc6a06bd08b1766365
Alan Lai <alan.fl.lai@gmail.com>
2018-04-19 22:15:53 +1000

    updated homepage and paragraph styling

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
     <Header :title="title" />
     <div class="content px-6 py-6">
       <p>My name is Alan Lai and I am a Front End Web Developer based in Sydney. I am interesting in adopting different FE technologies. At the moment I am focusing on Vue and React.</p>
-      <p class="pt-4">I am also interested in <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">Python</a>, <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">MongoDB</a> / <a href="http://mongoosejs.com/" target="_blank" rel="noopener noreferrer">MongooseJS</a> and <a href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer">NodeJS</a> / <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">ExpressJS</a>.</p>
+      <p class="mt-4">I am also interested in <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">Python</a>, <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">MongoDB</a> / <a href="http://mongoosejs.com/" target="_blank" rel="noopener noreferrer">MongooseJS</a> and <a href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer">NodeJS</a> / <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">ExpressJS</a>.</p>
     </div>
   </main>
 </template>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,6 +3,7 @@
     <Header :title="title" />
     <div class="content px-6 py-6">
       <p>This is an experimental website built with <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">VueJS</a> and <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">Tailwind</a>. Feel free to have a click around.</p>
+      <p class="mt-4">To view the static version of this website generated with NuxtJS <a href="https://demo-nuxt.alanlai.name/" target="_blank" rel="noopener noreferrer">click here</a>.</p>
     </div>
   </main>
 </template>
