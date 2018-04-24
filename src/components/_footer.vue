9349257dbc040e627f156fbc237084896809bb42
Alan Lai <alan.fl.lai@gmail.com>
2018-04-24 15:21:42 +1000

    added project link

---
+++ src/components/_footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="bg-black text-white mt-auto px-6 py-4">
+    <p>&copy; Alan Lai {{ year }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      year: null
+    }
+  },
+  beforeMount() {
+    let date = new Date();
+    this.year = date.getFullYear();
+  }
+}
+</script>
---
+++ src/components/_header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="px-6 py-6 bg-indigo-dark text-white">
+    <h1>{{ title }}</h1>  
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: ['title'],
+    data() {
+      return {
+      }
+    }
+  }
+</script>
